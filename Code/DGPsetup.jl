3510da7467e8d56e3bda22495d2411b95467ec20
loforteg <giulialoforte@gmail.com>
2021-03-22 17:14:08 -0700

    Create DGPsetup.jl

---
+++ Code/DGPsetup.jl
@@ -0,0 +1,29 @@
+# SET UP THE DATA GENERATING PROCESS
+"""
+This file loads the moments and sets up the Data Generating Process for step 3
+    of AFT2017.
+"""
+
+## Load packages
+using LinearAlgebra, Random, Distributions, Statistics, DataFrames
+using CSV, XLSX
+
+## Set seeds and directory
+Random.seed!(1234);
+cd("C:\\Users\\asus\\Desktop\\Giulia\\UBC\\Year2\\567 - Empirical IO\\AFT2017-Replication\\Code")
+
+
+## Assign country ID (check why!)
+chn_ctr_ind  = 59;
+ger_ctr_ind  = 28;
+can_ctr_ind  = 2;
+gbr_ctr_ind  = 22;
+mex_ctr_ind  = 3;
+twn_ctr_ind  = 62;
+ita_ctr_ind  = 39;
+jpn_ctr_ind  = 63;
+fra_ctr_ind  = 27;
+kor_ctr_ind  = 60;
+
+## Load input public data
+df = CSV.read("AER1moments2007.csv", DataFrame)
