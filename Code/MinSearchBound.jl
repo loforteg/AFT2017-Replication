9e0fecdc2879281350df342cfb758d7375838a53
loforteg <giulialoforte@gmail.com>
2021-03-23 17:27:21 -0700

    Create MinSearchBound.jl

---
+++ Code/MinSearchBound.jl
@@ -0,0 +1,10 @@
+# CONSTRUCT SEARCH FUNCTION
+
+
+## Load packages
+using LinearAlgebra, Random, Distributions, Statistics, DataFrames
+using CSV, XLSX, StatsBase
+
+## Set seeds and directory
+Random.seed!(1234);
+cd("C:\\Users\\asus\\Desktop\\Giulia\\UBC\\Year2\\567 - Empirical IO\\AFT2017-Replication\\Code")
