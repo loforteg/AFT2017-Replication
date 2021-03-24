04f65f790d69fe1a56737ff7628d85c81c456a8a
loforteg <giulialoforte@gmail.com>
2021-03-24 16:36:55 -0700

    Update JiaAlgorithm.jl

--- Code/JiaAlgorithm.jl
+++ Code/JiaAlgorithm.jl
@@ -18,8 +18,6 @@ my_exp = (σ-1)/θ
 
 fc_mean = fc_mean_guess[1] .* ((distrw').^fc_mean_guess[2]) .* fc_mean_guess[3].^(comlang') .* exp.(-fc_mean_guess[4] .* corrup')
 
-fc =
-
 temp = fc_shock_randn.*fc_disp_guess
 for i in 1:size(temp,1)
     for j in 1:size(temp,2)
@@ -40,63 +38,40 @@ source_check_lb = (check_matrix_lb * ξ).^my_exp
 
 firm = 1
 
-
-
-## Jia's lower bound algorithm
-#function lowerbound()
-
-# Start iteration for Marginal Benefit (MB)
-k = 1
-Z_start  = zeros(1, N)
 source_start = source_start_lb
 source_check = source_check_lb
 
-#while k < N
 
-# compute MB
-if k > 1
-    source_potential_shock_start = (Z_start * ξ).^my_exp
-    source_potential_shock_check = (Z_start * ξ + ξ .* (I - Z_start')).^my_exp
+## Jia's lower bound algorithm
+function lowerbound(source_start, source_check, ϕ_σ_B, fc, N, ξ, my_exp, firm)
+
+    # Start iteration for Marginal Benefit (MB)
+    k = 1
+    Z_start  = zeros(1, N)
+
+    while k < N
+
+    # compute MB
+        if k > 1
+            source_potential_shock_start = (Z_start * ξ).^my_exp
+            source_potential_shock_check = (Z_start * ξ + ξ .* (I - Z_start')).^my_exp
+        end
+
+        source_potential_start = ϕ_σ_B[firm] .* source_start
+        source_potential_new_vec = ϕ_σ_B[firm] .* source_check
+
+        # generate matrix with 1 if MB positive and update set of sourcing countries
+        MB = (source_potential_new_vec' - fc[firm, :] - source_potential_start .> 0)
+        Z_new = minimum(Z_start + MB, dims = 1)
+
+        if Z_start == Z_new
+            @goto end_lb_algorithm
+        end
+
+        k += 1
+        Z_start = Z_new
+    end
+
+    @label end_lb_algorithm
+    return Z_new
 end
-
-source_potential_start = ϕ_σ_B[firm] .* source_start
-source_potential_new_vec = ϕ_σ_B[firm] .* source_check
-
-# generate matrix with 1 if MB positive and update set of sourcing countries
-MB = (source_potential_new_vec' - fc[firm, :] - source_potential_start > 0)
-Z_new = minimum(Z_start + MB, dims = 1)
-
-if Z_start == Z_new
-    @goto end_lb_algorithm
-end
-
-k += k
-Z_start = Z_new
-#end
-@label end_lb_algorithm
-
-# return Z_new
-#end
-
-
-
-## MATLAB CODE
-while k <= Num_ctr
-    % Calculate Marginal Benefit =
-
-if k>1
-    source_potential_shock_mat_start  = (Z_start * exp_fe_est).^my_exponent;
-    source_potential_shock_mat_check = (Z_start * exp_fe_est+ exp_fe_est.*(1-Z_start')).^my_exponent;
-end
-    source_potential_start = phi_sigma_1_B_scalar * source_potential_shock_mat_start;  % scalar
-    source_potential_new_vec = phi_sigma_1_B_scalar * source_potential_shock_mat_check;  %m.num_loc_strings x1
-
-    % Check if MB is positive
-    MB_positive = (source_potential_new_vec' - fc_vec - source_potential_start >0);
-    Z_new = min(Z_start + MB_positive,1);
-    if Z_start==Z_new
-        break
-    end;
-    k=k+1;
-    Z_start = Z_new;
-end;
