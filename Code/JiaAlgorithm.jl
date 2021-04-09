754ed9513625e2098041d717b39aafd321bc98d2
loforteg <giulialoforte@gmail.com>
2021-04-09 10:16:44 -0700

    Update JiaAlgorithm.jl
    Create function optimalset returning the optimal set of sourcing countries using Jia LowerBound and UpperBound

--- Code/JiaAlgorithm.jl
+++ Code/JiaAlgorithm.jl
@@ -133,41 +133,51 @@ Z_ub = upperbound(source_start_ub, source_check_ub, ϕ_σ_B, fc, N, ξ, my_exp, 
 
 ## Check if Jia's algorithm produce the same lower and upper bound
 
-Z = 1.0*ones(S,N)
-gap_bounds = 1.0*ones(S,1)
+function optimalset(firm, Z_lb, Z_ub, S, N, num_rand_checks, rand_check_matrix, fc, ξ, my_exp, ϕ_σ_B)
 
-if Z_lb == Z_ub
-     Z[firm,:] = Z_lb
+    Z = 1.0*ones(S,N)
+    gap_bounds = 1.0*ones(S,1)
 
-# I don't know how to do lines 64-88 of gmm_objective.m: the case when lower and
-# upper bounds are not too different
+    if Z_lb == Z_ub
+        Z[firm,:] = Z_lb
 
-else
-    print("WARNING! The sourcing strategy may not be solved correctly")
-    Z_check = repeat(Z_lb, num_rand_checks, 1)
-    ind_diffZ = zeros(1,N)
-    for i in 1:N
-        if Z_ub[i] == Z_lb[i]
-            ind_diffZ[i] = 0.0
-        else
-            ind_diffZ[i] = 1.0
+    # I don't know how to do lines 64-88 of gmm_objective.m: the case when lower and
+    # upper bounds are not too different
+
+    else
+        print("WARNING! The sourcing strategy may not be solved correctly")
+        Z_check = repeat(Z_lb, num_rand_checks, 1)
+        ind_diffZ = zeros(1,N)
+        for i in 1:N
+            if Z_ub[i] == Z_lb[i]
+                ind_diffZ[i] = 0.0
+            else
+                ind_diffZ[i] = 1.0
+            end
         end
-    end
-    gap_bounds[firm] = sum(ind_diffZ)
-    K_top = Int(sum(ind_diffZ))
-    K_diff = [i[2] for i in findall(x->x!=0.0, ind_diffZ)]
-    for K in 1:K_top
-        Z_check[:,K_diff[K]] = (rand_check_matrix[:,K_diff[K]] > 0.5)
+        gap_bounds[firm] = sum(ind_diffZ)
+        K_top = Int(sum(ind_diffZ))
+        K_diff = [i[2] for i in findall(x->x!=0.0, ind_diffZ)]
+        for K in 1:K_top
+            Z_check[:,K_diff[K]] = (rand_check_matrix[:,K_diff[K]] > 0.5)
+        end
+
+        # Use the check and both bounds to find new set of sourcing countries
+        Z_check = [Z_check; Z_lb; Z_ub]
+        fc_payments = sum(Z_check .* fc[firm,:]', dims=2)
+        source_potential_shock_mat_check = (Z_check * ξ).^my_exp
+        source_potential_vec = ϕ_σ_B[firm] .* source_potential_shock_mat_check
+        tot_profit = source_potential_vec - fc_payments
+        loc_firm_best = findmax(tot_profit, dims = 1)[2]
+
+        # replace Z as the set of locations from Z_check that maximize profits
+        Z[firm,:] = Z_check[loc_firm_best[1][1],:]
     end
 
-    # Use the check and both bounds to find new set of sourcing countries
-    Z_check = [Z_check; Z_lb; Z_ub]
-    fc_payments = sum(Z_check .* fc[firm,:]', dims=2)
-    source_potential_shock_mat_check = (Z_check * ξ).^my_exp
-    source_potential_vec = ϕ_σ_B[firm] .* source_potential_shock_mat_check
-    tot_profit = source_potential_vec - fc_payments
-    loc_firm_best = findmax(tot_profit, dims = 1)[2]
-
-    # replace Z as the set of locations from Z_check that maximize profits
-    Z[firm,:] = Z_check[loc_firm_best[1][1],:]
+    return Z
 end
+
+
+## Check that it works
+firm = 138
+Z = optimalset(firm, Z_lb, Z_ub, S, N, num_rand_checks, rand_check_matrix, fc, ξ, my_exp, ϕ_σ_B)
