02b681cc7413542f89ea7ef2ab2ebf1a60990038
loforteg <giulialoforte@gmail.com>
2021-04-10 18:28:19 -0700

    Delete MinSearchBound.jl

--- Code/MinSearchBound.jl
+++
@@ -1,9 +0,0 @@
-# CONSTRUCT SEARCH FUNCTION
-
-
-## Load packages
-using LinearAlgebra, Random, Distributions, Statistics, DataFrames, StatsBase
-
-## Set seeds and directory
-Random.seed!(1234);
-cd("C:\\Users\\asus\\Desktop\\Giulia\\UBC\\Year2\\567 - Empirical IO\\AFT2017-Replication\\Code")
